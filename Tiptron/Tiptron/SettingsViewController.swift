3d7472455ff1d175f8aec7af94acbfc170ed1ec2
Rajeev Ram <rajeevram@email.arizona.edu>
2018-07-02 18:30:51 -0700

    adding extra features

--- Tiptron/Tiptron/SettingsViewController.swift
+++ Tiptron/Tiptron/SettingsViewController.swift
@@ -13,12 +13,16 @@ class SettingsViewController: UIViewController {
     // UI Variables
     @IBOutlet var background: UIView!
     @IBOutlet weak var themeChoices: UISegmentedControl!
+    @IBOutlet weak var customTipAmount: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
     
     // Initial View Controller
     var mainViewController: MainViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        themeChoices.selectedSegmentIndex = (mainViewController?.getThemeNumber())!
+        changeTheme()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,21 +30,38 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func newThemeChosen(_ sender: Any) {
+    func changeTheme() {
         if (themeChoices.selectedSegmentIndex == 0) {
             background.backgroundColor = UIColor(red: 1, green: 0.87, blue: 0.74, alpha: 1)
             themeChoices.tintColor = UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)
-            //mainView.checkAmount.textColor = UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)
         }
         else if (themeChoices.selectedSegmentIndex == 1) {
             background.backgroundColor = UIColor(red: 0.76, green: 0.98, blue: 0.85, alpha: 1)
             themeChoices.tintColor = UIColor(red: 0, green: 0.4, blue: 0.28, alpha: 1)
-            //mainView.checkAmount.textColor = UIColor(red: 0, green:0.4, blue: 0.28, alpha: 1)
         }
         else {
             background.backgroundColor = UIColor(red: 0.64, green: 0.69, blue: 0.76, alpha: 1)
             themeChoices.tintColor = UIColor(red: 0.25, green: 0.25, blue: 0.3, alpha: 1)
         }
     }
+    
+    @IBAction func newThemeChosen(_ sender: Any) {
+        changeTheme()
+        mainViewController?.setThemeNumber(index: themeChoices.selectedSegmentIndex)
+    }
 
+    @IBAction func pressEnterCustom(_ sender: Any) {
+        let newPercentage = Int(customTipAmount.text!) ?? -1
+        if (newPercentage > 0) {
+            mainViewController?.tipPercentage.removeSegment(at: 3, animated: false)
+            mainViewController?.tipPercentage.insertSegment(withTitle: "\(newPercentage)%", at: 3, animated: false)
+            mainViewController?.percentages[3] = Double(newPercentage) * 0.01
+            messageLabel.text = "Custom percentage replaced!"
+            mainViewController?.tipPercentage.selectedSegmentIndex = 3
+        }
+        else {
+            messageLabel.text = "Please enter valid integer."
+        }
+        
+    }
 }
--- Tiptron/Tiptron/ViewController.swift
+++ Tiptron/Tiptron/ViewController.swift
@@ -15,9 +15,27 @@ class MainViewController: UIViewController {
     @IBOutlet weak var tipAmount: UILabel!
     @IBOutlet weak var totalAmount: UILabel!
     @IBOutlet weak var tipPercentage: UISegmentedControl!
+    @IBOutlet var background: UIView!
+    
+    // Settings Variables
+    var themeNumber = 0
+    var percentages = [0.15,0.18,0.2,0.22]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        matchSelectedTheme()
+        calculateValues()
+    }
+    
+    func setThemeNumber(index: Int) {
+        themeNumber = index
+    }
+    
+    func getThemeNumber() -> Int {
+        return themeNumber
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,14 +49,53 @@ class MainViewController: UIViewController {
         //checkAmount.textColor = UIColor.gray
     }
     
+    func calculateValues() {
+        if (checkAmount.text == "") {
+            tipAmount.text = ""
+            totalAmount.text = ""
+        }
+        else {
+            let check = Double(checkAmount.text!) ?? 0
+            let tip = check * percentages[tipPercentage.selectedSegmentIndex]
+            let total = check + tip
+            tipAmount.text = String(format:"+\t$%.2f",tip)
+            totalAmount.text = String(format:"=\t$%.2f",total)
+        }
+    }
+    
     // Event handler for changing tip and total based on value entered
     @IBAction func changeValues(_ sender: Any) {
-        let check = Double(checkAmount.text!) ?? 0
-        let percentages = [0.15,0.18,0.2,0.22]
-        let tip = check * percentages[tipPercentage.selectedSegmentIndex]
-        let total = check + tip
-        tipAmount.text = String(format:"+\t$%.2f",tip)
-        totalAmount.text = String(format:"=\t$%.2f",total)
+        calculateValues()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let settingsView = segue.destination as? SettingsViewController
+        settingsView?.mainViewController = self
+    }
+    
+    func matchSelectedTheme() {
+        if (themeNumber == 0) {
+            background.backgroundColor = UIColor(red: 1, green: 0.87, blue: 0.74, alpha: 1)
+            tipPercentage.tintColor = UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)
+            checkAmount.textColor = UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)
+            tipAmount.textColor = UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)
+            totalAmount.textColor = UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)
+        }
+        else if (themeNumber == 1) {
+            background.backgroundColor = UIColor(red: 0.76, green: 0.98, blue: 0.85, alpha: 1)
+            tipPercentage.tintColor = UIColor(red: 0, green: 0.4, blue: 0.28, alpha: 1)
+            checkAmount.textColor = UIColor(red: 0, green: 0.4, blue: 0.28, alpha: 1)
+            tipAmount.textColor = UIColor(red: 0, green: 0.4, blue: 0.28, alpha: 1)
+            totalAmount.textColor = UIColor(red: 0, green: 0.4, blue: 0.28, alpha: 1)
+        }
+        else {
+            background.backgroundColor = UIColor(red: 0.64, green: 0.69, blue: 0.76, alpha: 1)
+            tipPercentage.tintColor = UIColor(red: 0.25, green: 0.25, blue: 0.3, alpha: 1)
+            checkAmount.textColor = UIColor(red: 0.25, green: 0.25, blue: 0.3, alpha: 1)
+            tipAmount.textColor = UIColor(red: 0.25, green: 0.25, blue: 0.3, alpha: 1)
+            totalAmount.textColor = UIColor(red: 0.25, green: 0.25, blue: 0.3, alpha: 1)
+        }
+    }
+    
 }
 
